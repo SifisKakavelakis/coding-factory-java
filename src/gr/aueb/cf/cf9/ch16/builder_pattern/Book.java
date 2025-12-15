3cf14bbcad2b4ea0cb005259d900dfd31ec110cd
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2025-12-15 19:56:17 +0200

    feat: add builder_pattern

---
+++ src/gr/aueb/cf/cf9/ch16/builder_pattern/Book.java
@@ -0,0 +1,4 @@
+package gr.aueb.cf.cf9.ch16.builder_pattern;
+
+public class Book {
+}
---
+++ src/gr/aueb/cf/cf9/ch16/builder_pattern/Main.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.cf9.ch16.builder_pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
